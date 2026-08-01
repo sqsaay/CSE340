046d765ff1eac855ec2fed1596ce132525888733
sqsaay <ayeleth.quevedo@gmail.com>
2026-08-01 15:02:12 -0600

    fix(validation_project): W04 Learning Activity: Repeating the Process: Inserting New Service Projects

--- src/models/projects.js
+++ src/models/projects.js
@@ -72,4 +72,24 @@ const getProjectDetails = async (id) => {
   return result.rows[0] ?? null;
 };
 
-export { getAllServiceProjects, getProjectsByOrganizationId, getUpcomingProjects, getProjectDetails };
+const createProject = async (title, description, location, date, organizationId) => {
+    const query = `
+      INSERT INTO project (title, description, location, date, organization_id)
+      VALUES ($1, $2, $3, $4, $5)
+      RETURNING project_id;
+    `;
+
+    const queryParams = [title, description, location, date, organizationId];
+    const result = await db.query(query, queryParams);
+
+    if (result.rows.length === 0) {
+        throw new Error('Failed to create project');
+    }
+
+    if (process.env.ENABLE_SQL_LOGGING === 'true') {
+        console.log('Created new project with ID:', result.rows[0].project_id);
+    }
+
+    return result.rows[0].project_id;
+}
+export { getAllServiceProjects, getProjectsByOrganizationId, getUpcomingProjects, getProjectDetails, createProject };
--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import express from 'express';
 
 import { homePage } from './controllers/index.js';
 import { organizationsPage, showOrganizationDetailsPage, showNewOrganizationForm, processNewOrganizationForm, organizationValidation, showEditOrganizationForm, processEditOrganizationForm } from './controllers/organizations.js';
-import { showProjectsPage, showProjectDetailsPage } from './controllers/projects.js';
+import { showProjectsPage, showProjectDetailsPage, showNewProjectForm, processNewProjectForm, projectValidation } from './controllers/projects.js';
 import { categoriesPage, showCategoryDetailsPage } from './controllers/categories.js';
 import { testErrorPage } from './controllers/errors.js';
 
@@ -17,6 +17,11 @@ router.get('/categories', categoriesPage);
 router.get('/category/:id', showCategoryDetailsPage);
 router.get('/new-organization', showNewOrganizationForm);
 router.get('/edit-organization/:id', showEditOrganizationForm); // Placeholder for edit organization form
+// Route for new project page
+router.get('/new-project', showNewProjectForm);
+// Route to handle new project form submission
+router.post('/new-project', projectValidation, processNewProjectForm);
+
 // Route to handle the edit organization form submission
 router.post('/edit-organization/:id', organizationValidation, processEditOrganizationForm);
 // Route to handle new organization form submission
